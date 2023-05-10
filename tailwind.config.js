e6befa364537e4b9b6bf405f7cc406aadb24aebf
Maduhansi Lakshini <maduhansilakshini@gmail.com>
2023-05-10 13:39:21 +0900

    app built

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,22 @@ module.exports = {
     "./src/pages/**/*.{js,ts,jsx,tsx}",
     "./src/components/**/*.{js,ts,jsx,tsx}",
   ],
+  safelist: [
+    'from-yellow-200',
+    'from-yellow-300',
+    'from-yellow-400',
+    'from-red-500',
+    'from-orange-500',
+    'from-yellow-500',
+    'from-green-500',
+    'from-cyan-500',
+    'from-blue-500',
+    'from-indigo-500',
+    'from-violet-500',
+    'from-purple-500',
+    'from-pink-500',
+  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}", "./index.html"],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
